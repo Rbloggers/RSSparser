0cf76ece60346e10d1b190cb496d76463a75e7a6
liao <liaomovie2@gmail.com>
2018-11-10 23:53:41 +0800

    typo

--- index.sh
+++ index.sh
@@ -9,7 +9,7 @@ _EOF_
 
 while read p; do
     dir=$(echo "$p" | cut -d ',' -f 1)
-    author=$(echo "$p" | cut -d ',' -f 1)
+    author=$(echo "$p" | cut -d ',' -f 2)
     printf "$author ([new](${dir}/new.json), [old](${dir}/old.json), [post](${dir}/new_post.json))\n\n" >> index.md
     
 done < authorlist.txt
