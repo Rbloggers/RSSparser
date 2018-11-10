bec9b12c560f2cf06a8ee77b5d4d175afbbfc36c
liao <liaomovie2@gmail.com>
2018-11-10 23:25:50 +0800

    build index page

---
+++ index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat >> index.md  << _EOF_
+
+# List of JSON
+<br><br>
+
+_EOF_
+
+while read p; do
+    dir=$(echo "$p" | cut -d ',' -f 1)
+    author=$(echo "$p" | cut -d ',' -f 1)
+    printf "$author ([new](${dir}/new.json), [old](${dir}/old.json), [post](${dir}/new_post.json))\n\n" >> index.md
+    
+done < authorlist.txt
