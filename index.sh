6c66a5a162d3fa2322fa0214e023dfd98bb904ce
liao <liaomovie2@gmail.com>
2018-11-11 00:21:02 +0800

    tweak

--- index.sh
+++ index.sh
@@ -11,7 +11,7 @@ _EOF_
 while read p; do
     dir=$(echo "$p" | cut -d ',' -f 1)
     author=$(echo "$p" | cut -d ',' -f 2)
-    printf "* $author ([new](${dir}/new.json), [old](${dir}/old.json), [post](${dir}/new_post.json))\n\n" >> index.md
+    printf "$author ([new](${dir}/new.json), [old](${dir}/old.json), [post](${dir}/new_post.json))\n\n" >> index.md
     
 done < authorlist.txt
 
