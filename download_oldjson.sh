70ae74ac1215ebd223d70cc25d9b8cd1f9b0fffa
liao <liaomovie2@gmail.com>
2018-12-08 18:07:48 +0800

    up

--- download_oldjson.sh
+++ download_oldjson.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-
-
-
-
 while read p; do
     authorname=$(echo "$p" | cut -d ',' -f 1)
     
     # Check if dir exist
     [[ -d $authorname ]] || mkdir $authorname
     
-
     ################ Normal ####################
     # Download old data
-    curl --fail "https://rbloggers.github.io/web/authors/$authorname/new.json" > "$authorname/old.json"
+    curl --silent --show-error --fail "https://raw.githubusercontent.com/Rbloggers/RSSparser/gh-pages/$authorname/new.json" > "$authorname/old.json"
     ############################################
     
-    ####### Interupution: make old post empty ########
+    ######## Interupution: make old post empty ########
     > "$authorname/old.json"
     ##########################
 
