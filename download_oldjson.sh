d84705e47ca6ff5d7dc344756a1f1e847ae9d457
liao <liaomovie2@gmail.com>
2018-12-24 12:51:20 +0800

    renew RSS

--- download_oldjson.sh
+++ download_oldjson.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
 
 while read p; do
     authorname=$(echo "$p" | cut -d ',' -f 1)
@@ -8,11 +10,11 @@ while read p; do
     
     ################ Normal ####################
     # Download old data
-    curl --silent --show-error --fail "https://raw.githubusercontent.com/Rbloggers/RSSparser/gh-pages/$authorname/new.json" > "$authorname/old.json"
+    curl --silent --show-error --fail "https://raw.githubusercontent.com/Rbloggers/RSSparser/gh-pages/$authorname/new.json" > "$authorname/old.json" || printf "${RED}$authorname${NC} old.json\n\n"
     ############################################
     
     ######## Interupution: make old post empty ########
-    #> "$authorname/old.json"
+    > "$authorname/old.json"
     ##########################
 
 done < authorlist.txt
